f5a07a06718bc7ebcf77b238e174ab5d07e215f6
Alex Belnitskii <belnitskii@mail.ru>
2024-06-13 18:31:16 +0200

    HW1_2

--- src/main/java/ru/javawebinar/topjava/util/Dates.java
+++ src/main/java/ru/javawebinar/topjava/util/Dates.java
@@ -4,10 +4,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public final class Dates {
-    private Dates() {
-    }
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm");
 
     public static String formatLocalDateTime(LocalDateTime localDateTime) {
-        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd H:mm"));
+        return localDateTime.format(DATE_TIME_FORMATTER);
     }
 }

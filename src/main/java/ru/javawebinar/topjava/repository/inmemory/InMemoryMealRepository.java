f3de6feb1f86e3461f96c32f6977d96385d100f5
belnitskii <belnitskii@gmail.com>
2026-02-14 19:53:52 +0100

    Update InMemoryMealRepository, add check meal belong to user

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -3,44 +3,59 @@ package ru.javawebinar.topjava.repository.inmemory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> save(meal, SecurityUtil.authUserId()));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
+            meal.setUserId(userId);
             mealsMap.put(meal.getId(), meal);
             return meal;
         }
-        // handle case: update, but not present in storage
-        return mealsMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        Meal oldMeal = mealsMap.get(meal.getId());
+        if (oldMeal != null && oldMeal.getUserId() == userId) {
+            mealsMap.replace(meal.getId(), meal);
+        }
+        return null;
     }
 
     @Override
-    public boolean delete(int id) {
-        return mealsMap.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        Meal meal = mealsMap.get(id);
+        if (meal != null && meal.getUserId() == userId) {
+            return mealsMap.remove(id) != null;
+        }
+        return false;
     }
 
     @Override
-    public Meal get(int id) {
-        return mealsMap.get(id);
+    public Meal get(int id, int userId) {
+        Meal meal = mealsMap.get(id);
+        if (meal != null && meal.getUserId() == userId) {
+            return meal;
+        }
+        return null;
     }
 
     @Override
     public Collection<Meal> getAll() {
-        return mealsMap.values();
+        return mealsMap.values().stream().sorted(Comparator.comparing(Meal::getDateTime).reversed()).collect(Collectors.toList());
     }
 }
 
